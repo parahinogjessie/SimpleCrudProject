a2c9c1cff7815f198a9988242fd5bbaaa5e7a294
parahinogjessie <parahinogjessie.com>
2025-09-07 12:01:46 +0800

    Normalize line endings to LF

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h2>Register</h2>
+        <form action="">
+            <input type="text" placeholder="name">
+            <input type="text" placeholder="email">
+            <input type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+</body>
+</html>
