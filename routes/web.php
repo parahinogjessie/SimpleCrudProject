c181b47ea8e320d5dca8c05b2b3b786c258839d2
parahinogjessie <parahinogjessie.com>
2025-09-07 18:30:34 +0800

    I'm just adding comment to the functions

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('home');
 });
-
+//This is for Register route
 Route::post('/register', [UserController::class,'register']); 
+
+//This is for Login and Logout route
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
